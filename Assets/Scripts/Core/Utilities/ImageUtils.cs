e0649e0b829565914ed7e0e13ac7921c3307194d
Rohit777 <rohitrana.r07@gmail.com>
2020-11-05 22:49:20 +0530

    video player

--- Assets/Scripts/Core/Utilities/ImageUtils.cs
+++ Assets/Scripts/Core/Utilities/ImageUtils.cs
@@ -3,10 +3,11 @@ using DefaultCompany.Core.Singletons;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+using System.IO;
 
 public class ImageUtils : Singleton<ImageUtils>
 {
-    public IEnumerator CaptureScreenForImage(Canvas ignoredCanvas, string defaultGallery, string defaultScreenshotFileName, GameObject tick, RawImage[] mtexture, Action<string, Texture2D, CaptureType> callback = null)
+    public IEnumerator CaptureScreenForImage(Canvas ignoredCanvas, string defaultGallery, string defaultScreenshotFileName, GameObject tick, RawImage[] mtexture)
     {
         ignoredCanvas.enabled = false;
 
@@ -21,5 +22,21 @@ public class ImageUtils : Singleton<ImageUtils>
         mtexture[ARTap.Count].texture = screenshotTexture;
         ignoredCanvas.enabled = true;
         tick.SetActive(true);
+    }
+
+    public IEnumerator ShareCollageScreenForImage(GameObject ignoredCanvas, string defaultScreenshotFileName)
+    { 
+        ignoredCanvas.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+
+        Texture2D screenshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshotTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshotTexture.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, defaultScreenshotFileName);
+        File.WriteAllBytes(filePath, screenshotTexture.EncodeToPNG());
+        new NativeShare().AddFile(filePath).Share();
+        ignoredCanvas.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Manager/NonARController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultCompany.Core.Singletons;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class NonARController : Singleton<UIManager>
+{
+    [SerializeField]
+    private GameObject Button;
+
+    [SerializeField]
+    private string defaultScreenshotFileName = "Screenshot.png";
+
+    private string selectedBtn;
+
+
+
+    public VideoPlayer vid;
+    public VideoClip[] myclip;
+    private VideoClip selectedClip;
+
+    public void ShareCollage() => StartCoroutine(ImageUtils.Instance.ShareCollageScreenForImage(Button, defaultScreenshotFileName));
+
+
+    public void OnClicked(Button button)
+    {
+        switch (button.name)
+        {
+            case "Mi_TvBtn":
+                selectedClip = myclip[0];
+                break;
+            case "Mi_CameraBtn":
+                selectedClip = myclip[1];
+                break;
+            case "Mi_SpeakerBtn":
+                selectedClip = myclip[2];
+                break;
+            case "Mi_WaterPurifierBtn":
+                selectedClip = myclip[3];
+                break;
+            case "MI_AirPurifierBtn":
+                selectedClip = myclip[4];
+                break;
+            case "Mi_BulbBtn":
+                selectedClip = myclip[5];
+                break;
+            case "Mi_LampBtn":
+                selectedClip = myclip[6];
+                break;
+            case "Mi_SmartLampBtn":
+                selectedClip = myclip[7];
+                break;
+        }
+    }
+
+
+}
